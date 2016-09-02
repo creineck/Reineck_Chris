0c53ab375b913dc95d3f9b80496abfed4896d096
youngnass <reineck.chris@gmail.com>
2016-09-02 13:33:06 -0700

    Added Files

---
+++ APLesson_01/secondProg.java
@@ -0,0 +1,13 @@
+public class secondProg 
+{
+	public static void main(String[]args) 
+	{
+		System.out.println("      __");
+		System.out.println("  _( \"\"  )_");
+		System.out.println("(_    .    _)");
+		System.out.println("  /    :   \\");
+		System.out.println(" (_/     \\_)");
+	}
+	
+	
+}
