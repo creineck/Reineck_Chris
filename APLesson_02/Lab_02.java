6842b6e10313a8ea0de37b0929449fe5385fca47
youngnass <reineck.chris@gmail.com>
2016-09-07 14:10:32 -0700

    Lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,31 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 8;
+		int num2 = 9;
+		int mult = num1 * num2;
+		
+		System.out.println(num1+" multiplied by "+num2+" is"+mult);
+		
+		System.out.println("\n");
+		
+		String name = "Chris \n";
+		String address = "505 Amigo Lane, ";
+		String city = "Del Mar \n";
+		String zip = "92015";
+		
+		System.out.println(name+address+city+zip);
+		
+		int l = 8;
+		int w = 5;
+		int h = 10;
+		
+		int surArea = (((l*w)+(l*h)+(w*h))*2);
+		
+		System.out.println("\n");
+		System.out.println("The surface area of a rectangular prism with a height of "+h+", \n");
+		System.out.println("a width of "+w+", and a length of "+ l+" is "+surArea+" units squared.");
+	}
+	
+}
