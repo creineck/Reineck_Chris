785a73793e7aa0913dd923550675b0c75ad2caa2
youngnass <reineck.chris@gmail.com>
2016-11-30 14:32:32 -0800

    lab 10

--- APLesson_10/Lab_10_Composites.java
+++ APLesson_10/Lab_10_Composites.java
@@ -1,26 +1,24 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
 public class Lab_10_Composites {
-	static Scanner kb;
+	static ArrayList<Integer> nums;
 	public static void main(String[]args) {
-		kb = new Scanner(System.in);
-		int[] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79};
-		ArrayList<Integer> nums = new ArrayList<>(numbers.asList(numbers));
+		Integer[] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79};
+		nums = new ArrayList<>(Arrays.asList(numbers));
+		removePrimes();
+		System.out.println(nums);
 	}
-	public static void gFactor(int num) {
-		System.out.println("Please input a number:");
-		int n = kb.nextInt();
-		for (int i = 2; i < n-1; i++) {
+	public static int gFactor(int n) {
+		for (int i = 2; i < n; i++) {
 			if (n%i == 0)
 				return 1;
-			else 
-				return 0;
 		}
+		return 0;
 	}
-	public static void removeComposites() {
-		for (int i = 0; i < nums.length; i++) {
-			if (gFactor()== 0)
-				
+	public static void removePrimes() {
+		for (int i = 0; i < nums.size(); i++) {
+			if (gFactor(nums.get(i))== 0)
+				nums.remove(i);
 		}
 	}
 }
